9794ec50d85f4184c83308bf9e42af187d5982cc
Viet Ta <viet.ta@disqo.com>
2022-11-20 02:24:18 +0200

    - Added final experiment results, charts - Adding more text to thesis

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ We run the redescription mining algorithms (MAS and MAP) with the following base
         <value>0.5</value>
     </parameter>
 ```
-For MAS, `max_var_s0` and `max_var_s1` was set like this:
+For MAS, `max_var_s0` and `max_var_s1` were set like this:
 ```xml
     <parameter>
         <name>max_var_s0</name>
@@ -41,7 +41,7 @@ For MAS, `max_var_s0` and `max_var_s1` was set like this:
         <value>4</value>
     </parameter>
 ```
-For MAP, `max_var_s0` and `max_var_s1` was set like this:
+For MAP, `max_var_s0` and `max_var_s1` were set like this:
 ```xml
     <parameter>
         <name>max_var_s0</name>
@@ -93,3 +93,52 @@ This defines the maximum size of a frequent itemset (number of terms in a candid
 Looks like it's also being used to filter candidates in the filter process, however I think it's unnecessary since candidates in this stage has to pass that requirement already. // TODO verify this  
 For MAP case `s0` is used for data in the left-hand side, and `s1` for data in the right-hand side.
 For MAS, `s0` and `s1` will be added to use as the upper bound for the size of a candidate.
+
+# Observation
+## candstore_length
+<img src="./thesis-playground/plots-output/candstore_length.png" alt="candstore_length" width="60%">
+
+- FP-Growth tends to have the smallest value among the three => it's because for FPG, the filtering process is stricter, the candstore_length is somewhat similar to the `no_of_redescriptions`
+- `min_itm_in`, `min_itm_out` have a strong effect on MAS
+- `min_fin_acc` has no effect on MAS and MAP but some effect on FPG, it's expected as MAS and MAP don't use it, and FPG use it. 
+- `max_var_s0` and `max_var_s1` has strong effect on MAP
+
+## no_of_queries, no_of_redescriptions
+|<img src="./thesis-playground/plots-output/no_of_queries.png" alt="no_of_queries"> | <img src="./thesis-playground/plots-output/no_of_redescriptions.png" alt="no_of_redescriptions"> |
+|---|---|
+|   |   |
+
+They should be the same?
+- MAP and FPG are identical, but MAS isn't, maybe it's because of the `max_var_s0` and `max_var_s1` differences, should run the experiment again for MAS with smaller `max_var`.
+
+=> After running the algorithm again, all the values are equal as expected. 
+
+## queries_support_min, queries_support_max, queries_support_mean, queries_support_median
+| <img src="./thesis-playground/plots-output/queries_support_min.png" alt="no_of_redescriptions">  | <img src="./thesis-playground/plots-output/queries_support_max.png" alt="no_of_redescriptions"> |
+|--------------------------------------------------------------------------------------------------|------------------------------------------------------------------------------------------------------------|
+| <img src="./thesis-playground/plots-output/queries_support_mean.png" alt="no_of_redescriptions"> | <img src="./thesis-playground/plots-output/queries_support_median.png" alt="no_of_redescriptions"> |
+
+- `queries_support_min`, `queries_support_max` are identical as expected
+- `queries_support_mean` and `queries_support_median` are not identical, why?
+
+## queries_accuracy_min, queries_accuracy_max, queries_accuracy_mean, queries_accuracy_median
+| <img src="./thesis-playground/plots-output/queries_accuracy_min.png" alt="no_of_redescriptions">  | <img src="./thesis-playground/plots-output/queries_accuracy_max.png" alt="no_of_redescriptions">    |
+|---------------------------------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------|
+| <img src="./thesis-playground/plots-output/queries_accuracy_mean.png" alt="no_of_redescriptions"> | <img src="./thesis-playground/plots-output/queries_accuracy_median.png" alt="no_of_redescriptions"> |
+
+## mining_duration
+<img src="./thesis-playground/plots-output/mining_duration.png" alt="mining_duration" width="60%">
+
+- Minidng duration for MAP is much smaller because, this's expected as MAP run on smaller datasets.
+- NOTE: For FPG, `mining_duration` and `filtering_duration` are combined
+## filtering_duration
+<img src="./thesis-playground/plots-output/filtering_duration.png" alt="filtering_duration" width="60%">
+
+- filtering duration for MAS is almost zero because it only need to go through all the frequent-itemsets once and check if one is an acceptable redescription or not.
+- NOTE: For FPG, `mining_duration` and `filtering_duration` are combined
+
+## mining_and_filtering_duration
+<img src="./thesis-playground/plots-output/mining_and_filtering_duration.png" alt="mining_and_filtering_duration" width="60%">
+
+Sum of mining and filtering duration for comparison with FPG algorithm
+- 
--- thesis-playground/plotting.py
+++ thesis-playground/plotting.py
@@ -2,20 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_simple(data, x_label, y_label, ax=None, line_label=None):
+def plot_simple(data, x_label, y_label, ax=None, line_label=None, line_style='solid'):
     x = data[x_label]
     y = data[y_label]
 
     if ax is None:
         fig, ax = plt.subplots()
-        ax.plot(x, y, label='mas')
+        ax.plot(x, y, label='mas', linestyle=line_style)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
         plt.axis([np.min(x), np.max(x), np.min(y), np.max(y)])
         plt.show()
         return
 
-    ax.plot(x, y, label=line_label)
+    ax.plot(x, y, label=line_label, linestyle=line_style)
+    # ax.set_ylim(bottom=0)
     ax.set_title(x_label)
     ax.legend()
 
@@ -25,32 +26,55 @@ def plot_group(target_metric):
 
     plt.suptitle(target_metric)
     plot_simple(min_itm_in_mas_data, 'min_itm_in', target_metric, axes[0][0], 'MAS')
+    # plot_simple(min_itm_in_mas_old_data, 'min_itm_in', target_metric, axes[0][0], 'MAS Old')
     plot_simple(min_itm_in_map_data, 'min_itm_in', target_metric, axes[0][0], 'MAP')
     plot_simple(min_itm_in_fp_growth_data, 'min_itm_in', target_metric, axes[0][0], 'FP Growth')
+    axes[0][0].set_ylim(bottom=0, top=None, emit=True, auto=True)
 
     plot_simple(min_itm_out_mas_data, 'min_itm_out', target_metric, axes[0][1], 'MAS')
+    # plot_simple(min_itm_out_mas_old_data, 'min_itm_out', target_metric, axes[0][1], 'MAS Old')
     plot_simple(min_itm_out_map_data, 'min_itm_out', target_metric, axes[0][1], 'MAP')
     plot_simple(min_itm_out_fp_growth_data, 'min_itm_out', target_metric, axes[0][1], 'FP Growth')
+    axes[0][1].set_ylim(bottom=0, top=None, emit=True, auto=True)
 
     plot_simple(min_fin_in_mas_data, 'min_fin_in', target_metric, axes[1][0], 'MAS')
+    # plot_simple(min_fin_in_mas_old_data, 'min_fin_in', target_metric, axes[1][0], 'MAS Old')
     plot_simple(min_fin_in_map_data, 'min_fin_in', target_metric, axes[1][0], 'MAP')
     plot_simple(min_fin_in_fp_growth_data, 'min_fin_in', target_metric, axes[1][0], 'FP Growth')
+    axes[1][0].set_ylim(bottom=0, top=None, emit=True, auto=True)
 
     # plot_simple(min_fin_out_mas_data, 'min_fin_out', target_metric, axes[1][1], 'MAS')
 
     plot_simple(min_fin_acc_mas_data, 'min_fin_acc', target_metric, axes[1][1], 'MAS')
+    # plot_simple(min_fin_acc_mas_old_data, 'min_fin_acc', target_metric, axes[1][1], 'MAS Old')
     plot_simple(min_fin_acc_map_data, 'min_fin_acc', target_metric, axes[1][1], 'MAP')
     plot_simple(min_fin_acc_fp_growth_data, 'min_fin_acc', target_metric, axes[1][1], 'FP Growth')
+    axes[1][1].set_ylim(bottom=0, top=None, emit=True, auto=True)
 
     plot_simple(max_var_s0_mas_data, 'max_var_s0', target_metric, axes[2][0], 'MAS')
+    # plot_simple(max_var_s0_mas_old_data, 'max_var_s0', target_metric, axes[2][0], 'MAS Old')
     plot_simple(max_var_s0_map_data, 'max_var_s0', target_metric, axes[2][0], 'MAP')
     plot_simple(max_var_s0_fp_growth_data, 'max_var_s0', target_metric, axes[2][0], 'FP Growth')
+    axes[2][0].set_ylim(bottom=0, top=None, emit=True, auto=True)
+    axes[2][0].autoscale(enable=True)
+
 
     plot_simple(max_var_s1_mas_data, 'max_var_s1', target_metric, axes[2][1], 'MAS')
+    # plot_simple(max_var_s1_mas_old_data, 'max_var_s1', target_metric, axes[2][1], 'MAS Old')
     plot_simple(max_var_s1_map_data, 'max_var_s1', target_metric, axes[2][1], 'MAP')
     plot_simple(max_var_s1_fp_growth_data, 'max_var_s1', target_metric, axes[2][1], 'FP Growth')
+    axes[2][1].set_ylim(bottom=0, top=None, emit=True, auto=True)
+
+    if target_metric == 'mining_and_filtering_duration':
+        plot_simple(min_itm_in_map_data, 'min_itm_in', 'mining_duration', axes[0][0], 'MAP Mining', 'dashed')
+        plot_simple(min_itm_out_map_data, 'min_itm_out', 'mining_duration', axes[0][1], 'MAP Mining', 'dashed')
+        plot_simple(min_fin_in_map_data, 'min_fin_in', 'mining_duration', axes[1][0], 'MAP Mining', 'dashed')
+        plot_simple(min_fin_acc_map_data, 'min_fin_acc', 'mining_duration', axes[1][1], 'MAP', 'dashed')
+        plot_simple(max_var_s1_map_data, 'max_var_s1', 'mining_duration', axes[2][1], 'MAP', 'dashed')
+        plot_simple(max_var_s0_map_data, 'max_var_s0', 'mining_duration', axes[2][0], 'MAP', 'dashed')
 
     plt.tight_layout()
+    # plt.ylim(bottom=0)
     plt.savefig('./plots-output/' + target_metric + '.png')
     plt.show()
 
@@ -59,12 +83,20 @@ mas_data = np.genfromtxt('./data/MAS-report.csv', delimiter=',', names=True)
 min_itm_in_mas_data = mas_data[0:10]
 min_itm_out_mas_data = mas_data[10:20]
 min_fin_in_mas_data = mas_data[20:30]
-min_fin_out_mas_data = mas_data[30:40]
-min_fin_acc_mas_data = mas_data[40:48]
-max_var_s0_mas_data = mas_data[48:52]
-max_var_s1_mas_data = mas_data[52:56]
+min_fin_acc_mas_data = mas_data[30:38]
+max_var_s0_mas_data = mas_data[38:42]
+max_var_s1_mas_data = mas_data[42:46]
 
-map_data = np.genfromtxt('./data/MAP-report.csv', delimiter=',', names=True)
+mas_old_data = np.genfromtxt('data/MAS-old-report.csv', delimiter=',', names=True)
+min_itm_in_mas_old_data = mas_old_data[0:10]
+min_itm_out_mas_old_data = mas_old_data[10:20]
+min_fin_in_mas_old_data = mas_old_data[20:30]
+min_fin_out_mas_old_data = mas_old_data[30:40]
+min_fin_acc_mas_old_data = mas_old_data[40:48]
+max_var_s0_mas_old_data = mas_old_data[48:52]
+max_var_s1_mas_old_data = mas_old_data[52:56]
+
+map_data = np.genfromtxt('data/MAP-report.csv', delimiter=',', names=True)
 min_itm_in_map_data = map_data[0:10]
 min_itm_out_map_data = map_data[10:20]
 min_fin_in_map_data = map_data[20:30]
@@ -94,6 +126,7 @@ target_metrics = [
     "queries_accuracy_median",
     "mining_duration",
     "filtering_duration",
+    "mining_and_filtering_duration",
 ]
 
 for target_metric in target_metrics:
